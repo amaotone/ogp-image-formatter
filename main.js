b068a8086005f1bceeb9eb1f6eecb84fae6e0f96
amaotone <amane.suzu@gmail.com>
2019-07-10 12:45:51 +0900

    Fix resize

--- main.js
+++ main.js
@@ -4,15 +4,34 @@ window.onload = function () {
   var cropper = window.Cropper;
   var URL = window.URL || window.webkitURL;
   var image = document.getElementById('preview');
+  var aspect = 1200 / 630;
   var options = {
-    aspectRatio: 1200 / 630,
-    cropBoxData: { width: 1200, height: 630 },
+    aspectRatio: aspect,
     preview: '#preview',
     minContainerWidth: 600,
     minContainerHeight: 315,
     minCropBoxWidth: 600,
     minCropBoxHeight: 315,
     autoCropArea: 1,
+    ready: function () {
+      // adjust width
+      if (image.naturalWidth > 1200) {
+        cropper.zoomTo(1200 / image.naturalWidth);
+        cropper.setCropBoxData({ width: 1200, height: 315 });
+      } else if (image.width < 600) {
+        cropper.zoomTo(600 / image.naturalWidth);
+        cropper.setCropBoxData({ width: 600, height: 315 });
+      }
+      var cropBox = cropper.getCropBoxData();
+      var left = (1200 - cropBox.width) / 2;
+      var top = (800 - cropBox.height) / 2;
+      cropper.setCropBoxData({ left: left, top: top })
+      console.log(cropper.getImageData());
+      console.log(cropper.getCropBoxData());
+    },
+    crop: function (e) {
+      console.log(cropper.getCropBoxData());
+    }
   }
   var cropper = new Cropper(image, options);
 
@@ -31,14 +50,19 @@ window.onload = function () {
   }
 
   var button = document.getElementById('download');
-  button.onclick = function (event) {
+  button.onclick = function () {
     var link = document.createElement("a");
+    var cropBox = cropper.getCropBoxData();
     link.href = cropper.getCroppedCanvas({
+      width: cropBox.width,
+      height: cropBox.height,
       minWidth: 600,
       minHeight: 315,
       maxWidth: 1200,
       maxHeight: 630,
-      fillColor: '#ffffff'
+      fillColor: '#ffffff',
+      imageSmoothingEnabled: true,
+      imageSmoothingQuality: 'high',
     }).toDataURL('image/jpeg');
     link.download = 'image.jpg';
     link.click();
